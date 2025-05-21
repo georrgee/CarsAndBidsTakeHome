b7ab49fb1614cb724e5d7dc6c541304eaa7ff2cd
George Garcia <garcia.george.b@gmail.com>
2025-05-21 12:15:22 -0700

    Added Network Service
    - added a class called Auction Service where it handles on the API calling

--- CarsAndBidsAuctionApp/ContentView.swift
+++ CarsAndBidsAuctionApp/ContentView.swift
@@ -50,6 +50,39 @@ struct AuctionResponse: Codable {
     let meta: Meta
 }
 
+class AuctionService {
+    
+    static let shared = AuctionService()
+    
+    /* @description: fetches all auctions */
+    func fetchAuctions() async throws -> [Auction] {
+        
+        let url = URL(string: "https://sbffr.carsandbids.com/api/auction-groups/myvj0bjns2k55kvwuuukalb1?populate[0]=auctions.main_image")!
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let response = try JSONDecoder().decode(AuctionResponse.self, from: data)
+        
+        return response.data
+    }
+    
+    
+    /** @description: fetches an auction based on the id */
+    
+    func fetchAuctionById(auctionId: String) async throws -> Auction  {
+        
+        let url = URL(string: "https://sbffr.carsandbids.com/api/auctions?filters[auction_id][$eq]=\(auctionId)")!
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let response = try JSONDecoder().decode(AuctionResponse.self, from: data)
+        
+        guard let auction = response.data.first else {
+            throw URLError(.badServerResponse)
+        }
+        
+        return auction
+    }
+}
+
 struct ContentView: View {
 
     var body: some View {
