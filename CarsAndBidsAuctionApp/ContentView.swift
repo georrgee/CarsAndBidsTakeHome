a219401727f70e3e60f9f1875dec5a130c283478
George Garcia <garcia.george.b@gmail.com>
2025-05-21 12:00:55 -0700

    Defining the Models
    - added the following models: Auction, Meta, Pagination, AuctionResponse
    
    - using codable to handle both encoding and decoding
    - using CodingKey to map keys, for example:
    data: "num_bids" -> numBids

--- CarsAndBidsAuctionApp/ContentView.swift
+++ CarsAndBidsAuctionApp/ContentView.swift
@@ -3,6 +3,52 @@
 //  Created by George Garcia on 5/21/25
 
 import SwiftUI
+import Foundation
+
+struct Auction: Codable, Identifiable {
+    let id: Int
+    let documentId: String
+    let title: String
+    let subtitle: String
+    let auctionId: String
+    let createdAt: String
+    let updatedAt: String
+    let publishedAt: String
+    let numBids: Int
+    let numComments: Int
+    let highBid: Int
+    let auctionUrl: String
+    let auctionEnd: String
+    let auctionStatus: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, documentId, title, subtitle
+        case auctionId = "auction_id"
+        case createdAt, updatedAt, publishedAt
+        case numBids = "num_bids"
+        case numComments = "num_comments"
+        case highBid = "high_bid"
+        case auctionUrl = "auction_url"
+        case auctionEnd = "auction_end"
+        case auctionStatus = "auction_status"
+    }
+}
+
+struct Meta: Codable {
+    let pagination: Pagination
+}
+
+struct Pagination: Codable {
+    let page: Int
+    let pageSize: Int
+    let pageCount: Int
+    let total: Int
+}
+
+struct AuctionResponse: Codable {
+    let data: [Auction]
+    let meta: Meta
+}
 
 struct ContentView: View {
 
