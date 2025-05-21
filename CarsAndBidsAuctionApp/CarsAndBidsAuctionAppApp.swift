d60aa637b0f53d0c4f0de5b207b3e5b082738020
George Garcia <garcia.george.b@gmail.com>
2025-05-21 10:01:39 -0700

    Initial Commit

---
+++ CarsAndBidsAuctionApp/CarsAndBidsAuctionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsAndBidsAuctionAppApp.swift
+//  CarsAndBidsAuctionApp
+//
+//  Created by George Garcia on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct CarsAndBidsAuctionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
